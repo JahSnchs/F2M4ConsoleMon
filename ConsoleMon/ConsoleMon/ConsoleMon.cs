156b566ac2d78a322bd296437516dae009871356
aka <contact@akari.work>
2022-05-09 11:22:09 +0200

    Finished Classes & Functions + List

---
+++ ConsoleMon/ConsoleMon/ConsoleMon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+    internal class ConsoleMon
+    {
+        private int health;
+        private int energy;
+        private string name;
+
+        List<Skill> Skills = new List<Skill>();
+
+        internal void TakeDmg(int damage)
+        {
+            health -= damage;
+        }
+
+        internal void DepleteEnergy(int energyCost)
+        {
+            energy -= energyCost;
+        }
+
+
+    }
+}
---
+++ ConsoleMon/ConsoleMon/Skill.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+    internal class Skill
+    {
+        private int damage;
+        private int energyCost;
+        private string name;
+
+        internal void UseOn(ConsoleMon target, ConsoleMon caster)
+        {
+            caster.DepleteEnergy(energyCost);
+            target.TakeDmg(damage);
+        }
+    }
+}
